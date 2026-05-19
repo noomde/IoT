79eeca7d3a5f73658844be08827757910ec34abb
Anthon Bäckström <143720072+noomde@users.noreply.github.com>
2026-05-19 14:52:04 +0200

    Update settings.js

--- data/settings.js
+++ data/settings.js
@@ -3,6 +3,8 @@ module.exports = {
     credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
     flowFile: "flows.json",
     functionExternalModules: true,
+
+    disableEditor: true,
     
     ui: {
         path: "/"
